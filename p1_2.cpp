46a7af2d72d6614b63492bf553dcdd0c071718e1
imoore02 <isobelmoore02@gmail.com>
2023-06-02 21:45:59 +0930

    fix

--- p1_2.cpp
+++ p1_2.cpp
@@ -15,9 +15,9 @@ void p1(){
 		for (int dice_2 = 1; dice_2 < 7; dice_2++){
 			for (int dice_3 = 1; dice_3 < 7; dice_3++){
 				for (int dice_4 = 1; dice_4 < 7; dice_4++){
-					if (((dice_1 + dice_2 != 3 && dice_1 + dice_2 != 8 && dice_1 + dice_2 != 11) && (dice_3 + dice_4 != 3 && dice_3 + dice_4 != 8 && dice_3 + dice_4 != 11)) &&
-						((dice_1 + dice_3 != 3 && dice_1 + dice_3 != 8 && dice_1 + dice_3 != 11) && (dice_2 + dice_4 != 3 && dice_2 + dice_4 != 8 && dice_2 + dice_4 != 11)) &&
-						((dice_1 + dice_4 != 3 && dice_1 + dice_4 != 8 && dice_1 + dice_4 != 11) && (dice_2 + dice_3 != 3 && dice_2 + dice_3 != 8 && dice_2 + dice_3 != 11)))
+					if (((dice_1 + dice_2 != 2 && dice_1 + dice_2 != 4 && dice_1 + dice_2 != 11) && (dice_3 + dice_4 != 2 && dice_3 + dice_4 != 4 && dice_3 + dice_4 != 11)) &&
+						((dice_1 + dice_3 != 2 && dice_1 + dice_3 != 4 && dice_1 + dice_3 != 11) && (dice_2 + dice_4 != 2 && dice_2 + dice_4 != 4 && dice_2 + dice_4 != 11)) &&
+						((dice_1 + dice_4 != 2 && dice_1 + dice_4 != 4 && dice_1 + dice_4 != 11) && (dice_2 + dice_3 != 2 && dice_2 + dice_3 != 4 && dice_2 + dice_3 != 11)))
 						num++;
 				}
 			}
